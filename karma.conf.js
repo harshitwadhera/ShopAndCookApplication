e42cd2512c4e33646313f3b6478816cb684270ff
harshitwadhera <40739494+harshitwadhera@users.noreply.github.com>
2020-07-13 04:19:50 +0530

    sad

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ShopAndCookApplction'),
+      dir: require('path').join(__dirname, './coverage/ShopAndCookApplication'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ShopAndCookApplction'`, () => {
+  it(`should have as title 'ShopAndCookApplication'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShopAndCookApplction');
+    expect(app.title).toEqual('ShopAndCookApplication');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ShopAndCookApplction app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ShopAndCookApplication app is running!');
   });
 });
